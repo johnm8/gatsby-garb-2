e4931a5f6899a5d9bbb851957abac8fe8b20982d
weekendur <john@weekendur.com>
2019-07-16 19:54:38 +0100

    init commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onRouteUpdate = ({ location, action }) => {
+console.log('route changed!',{location , action});
+}
+// this file is for browser apis
